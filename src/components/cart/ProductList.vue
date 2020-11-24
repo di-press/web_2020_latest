877b56cd0cb5c23c1c00b35c60fc19a80f4212dc
anacst <ana_costa@usp.br>
2020-11-24 16:15:07 -0300

    Fixed cart function

--- src/components/cart/ProductList.vue
+++ src/components/cart/ProductList.vue
@@ -3,7 +3,7 @@
     <v-card outlined>
       <v-card-title>Produtos</v-card-title>
       <v-row v-for="product in products" :key="product.name">
-        <HorizontalProduct :product="product" :btnAction="remove" />
+        <HorizontalProduct :product="product" />
       </v-row>
     </v-card>
   </v-app>
@@ -16,29 +16,6 @@ export default {
   components: {
     HorizontalProduct,
   },
-  methods: {
-    removeFromCart() {
-      alert("aqui");
-      this.product.quantity = 0;
-    },
-  },
-  data: function () {
-    return {
-      products: [
-        {
-          name: "Moletom",
-          quantity: 1,
-          price: 120.9,
-          //image: require('../../assets/moletom.png'),
-        },
-        {
-          name: "Agenda 2021",
-          quantity: 1,
-          price: 79.6,
-          //image: require('../../assets/moletom.png'),
-        },
-      ],
-    };
-  },
+  props: ['products']
 };
 </script>
--- src/views/Cart.vue
+++ src/views/Cart.vue
@@ -6,11 +6,11 @@
     <v-cointainer>
       <v-row>
         <v-col sm="8" md="5" offset-md="2" lg="4" offset-lg="3">
-          <ProductList />
+          <ProductList :products="products" />
         </v-col>
 
         <v-col sm="4" md="3">
-          <CheckoutBox />
+          <CheckoutBox :totalPrice="totalPrice" :totalProducts="totalProducts" />
         </v-col>
       </v-row>
     </v-cointainer>
@@ -26,6 +26,36 @@ export default {
     ProductList,
     CheckoutBox,
   },
+  data: function () {
+    return {
+      products: [
+        {
+          name: "Moletom",
+          quantity: 1,
+          price: 180,
+          //image: require('../../assets/moletom.png'),
+        },
+        {
+          name: "Agenda 2021",
+          quantity: 1,
+          price: 80,
+          //image: require('../../assets/moletom.png'),
+        },
+      ],
+    };
+  },
+  computed: {
+    totalPrice() {
+      return this.products.reduce((sum, product) => {
+        return sum + product.quantity * product.price;
+      }, 0);
+    },
+    totalProducts() {
+      return this.products.reduce((sum, product) => {
+        return sum + product.quantity;
+      }, 0);
+    },
+  },  
 };
 </script>
 

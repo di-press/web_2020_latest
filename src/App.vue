1c5e92ab0c13686ca266af6c523a3c8f5020ea65
M-A-rs <araujo_m_@hotmail.com>
2020-12-10 00:17:04 -0300

    Corrigir cor no ícone do botão do carrinho

--- src/App.vue
+++ src/App.vue
@@ -49,7 +49,8 @@
         <span v-if="$route.name === 'Usuarios' || $route.name === 'Estoque'"
           >Usuários
         </span>
-        <v-badge content="1" value="1" color="red">
+        <!-- Tentar deixar dinâmico o número de produtos no ícone do carrinho -->
+        <v-badge content="2" value="1" color="#4dd0e1">
           <v-icon v-if="$route.name !== 'Usuarios' && $route.name !== 'Estoque'"
             >mdi-cart</v-icon
           >

16999b0eb8e3df4bdbf65f3848570bf64644f5d4
di-press <diany_press@usp.br>
2020-12-11 00:37:31 -0300

    completa remoção de produtos

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,37 @@
 <template>
   <v-main>
     <highlights />
-    <products tipo="NOVIDADES">teste</products>
-    <products tipo="PROMOÇÕES"></products>
-    <products tipo="CATEGORIAS"></products>
+    <products tipo="NOVIDADES" :produtos="novidades"></products>
+    <products tipo="PROMOÇÕES" :produtos="promocoes"></products>
+    <products tipo="EXCLUSIVOS" :produtos="exclusivos"></products>
   </v-main>
 </template>
 
 <script>
-//import Header from "./Header.vue";
 import highlights from "./highlights.vue";
 import products from "./ProductsShow.vue";
 
 export default {
   components: { products, highlights },
   data() {
-    return {};
+    return {
+      novidades: [],
+      promocoes: [],
+      exclusivos: []
+    };
   },
+  methods: {},
+  watch: {
+    '$store.state.novidades': function() {
+        this.novidades = this.$store.state.novidades;
+     },
+     '$store.state.promocoes': function() {
+        this.promocoes = this.$store.state.promocoes;
+     },
+     '$store.state.exclusivos': function() {
+        this.exclusivos = this.$store.state.exclusivos;
+     },
+  }
+
 };
 </script>
